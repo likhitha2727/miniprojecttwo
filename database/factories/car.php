82552dacd36f702639ccbe4a1eaf46b9394f94fe
sn596@njit.edu <Naralasetty@123>
2018-11-01 11:23:58 -0400

    Task: Created Factory

--- database/factories/car.php
+++ database/factories/car.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        //
+        'Make' => $faker->randomElement(['Ford','Honda','Toyota']),
+        'Model' => $faker->word,
+        'Year' => $faker->year($max = 'now'),
     ];
 });
