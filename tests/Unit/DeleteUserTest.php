c87ad18a9e0c2a2a0e8ad210794c251b6d410f5d
sn596@njit.edu <Naralasetty@123>
2018-11-01 22:07:48 -0400

    Task: Unit Test(Delete user in user table)

--- tests/Unit/DeleteUserTest.php
+++ tests/Unit/DeleteUserTest.php
@@ -1,11 +1,9 @@
 <?php
-
 namespace Tests\Unit;
-
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-
+use App\User;
 class DeleteUserTest extends TestCase
 {
     /**
@@ -13,8 +11,11 @@ class DeleteUserTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testDelete()
     {
-        $this->assertTrue(true);
+        //$this->assertTrue(true);
+        $user = User::find(50);
+        $user->delete();
+        $this->assertTrue($user->save());
     }
 }
